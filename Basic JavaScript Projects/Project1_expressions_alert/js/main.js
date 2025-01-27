574e06c68549e6efc13a92282db4c67f13bd83f4
“jackpipergit” <jpiper@cladco.co.uk>
2025-01-27 15:18:33 +0000

    Project1_expressions_alert

---
+++ Basic JavaScript Projects/Project1_expressions_alert/js/main.js
@@ -0,0 +1,15 @@
+// First part of our string
+var Sent1 = "This is the beginning of the string";
+
+// Second part of our string
+var Sent2 = " and this is the end of the string";
+
+// Creating an expression that adds 5 + 5
+var expression = 5 + 5;
+
+// Using window.alert() to display our concatenated string
+window.alert(Sent1 + Sent2);
+
+// Using document.write() to display our concatenated string and expression result
+document.write(Sent1 + Sent2 + "<br>");
+document.write("5 + 5 = " + expression);

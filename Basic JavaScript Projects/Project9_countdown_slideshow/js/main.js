08a37257b50eff09f7b3f741980ec36e30d153ff
“jackpipergit” <jpiper@cladco.co.uk>
2025-01-28 09:25:40 +0000

    Countdown Timer & Slideshow

---
+++ Basic JavaScript Projects/Project9_countdown_slideshow/js/main.js
@@ -0,0 +1,55 @@
+// Countdown timer function
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("Time's up!");
+            clearTimeout(time);
+            timer.innerHTML = "";
+        }
+    }
+    tick();
+}
+
+// Slideshow Functions
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+
+    // Loop back to first slide if we go past the end
+    if (n > slides.length) { slideIndex = 1 }
+    // Go to last slide if we go before the first
+    if (n < 1) { slideIndex = slides.length }
+
+    // Hide all slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    // Remove active class from all dots
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    // Show the current slide and activate the corresponding dot
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
